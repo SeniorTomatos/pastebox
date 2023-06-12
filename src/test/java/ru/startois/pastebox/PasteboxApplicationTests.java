0ab9402d43303c2255f82419915b70d3316a779f
SeniorTomatos <dehuckoo5@gmail.com>
2023-06-12 23:24:18 +0300

    Init

---
+++ src/test/java/ru/startois/pastebox/PasteboxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.startois.pastebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
